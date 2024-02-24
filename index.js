ed6cc9992c37ff048a01795b3494315848618c22
Andrea  López Chico <andrealopezchico@Andreas-MacBook-Pro.local>
2024-02-24 10:39:18 -0600

    se modifico la funcion app

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import express from 'express';
-import productController from './controllers/productsController.js';
+import express from "express";
+import productController from "./controllers/productsController.js";
 
 const app = express();
-app.use('/productos',productController);
-
+app.get("/", (req, res) => {
+  res.json({ message: "Bienvenidos a la app" });
+});
+app.use("/productos", productController);
 
 const port = 4000;
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
