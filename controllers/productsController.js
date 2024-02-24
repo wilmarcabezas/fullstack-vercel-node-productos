18ba670a269959515ce02d12011c51918d103855
Andrea  López Chico <andrealopezchico@Andreas-MacBook-Pro.local>
2024-02-24 11:08:29 -0600

    se agregaron funciones a controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,17 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Product List.'});
+router.get("/", (req, res) => {
+  res.json({ message: "Product List." });
 });
 
+router.get("/:id", (req, res) => {
+  res.json({ message: req.params.id });
+});
+
+router.post("/", (req, res) => {
+  res.json({ message: "Producto Registrado" });
+});
 
 export default router;
