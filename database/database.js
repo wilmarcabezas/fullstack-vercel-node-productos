b139f7e8c67a6fbffdd0fc324e9c6a63a78e79e5
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2024-03-02 11:33:21 -0500

    Despliegue

--- database/database.js
+++ database/database.js
@@ -3,9 +3,11 @@ import pkg from "pg";
 const { Client } = pkg;
 
 const connectionString =
-  "postgres://root01:FjiX2HEav00b430M7h53zLWxMyywHpWN@dpg-cnd2soen7f5s73bjn6v0-a/database01_bbz3";
+  "postgres://root01:FjiX2HEav00b430M7h53zLWxMyywHpWN@dpg-cnd2soen7f5s73bjn6v0-a.oregon-postgres.render.com/database01_bbz3";
+  //Externa:postgres://root01:FjiX2HEav00b430M7h53zLWxMyywHpWN@dpg-cnd2soen7f5s73bjn6v0-a.oregon-postgres.render.com/database01_bbz3
+  //Interna:postgres://root01:FjiX2HEav00b430M7h53zLWxMyywHpWN@dpg-cnd2soen7f5s73bjn6v0-a/database01_bbz3
 
-const client = new Client({ connectionString: connectionString });
+const client = new Client({ connectionString: connectionString,ssl:true },);
 
 const conectar = () => {
   client
